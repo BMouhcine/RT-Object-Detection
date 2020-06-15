aa3cc165b6ee80fbde4f802f2db8c092a9f12fd3
Ouokki <ouokki98@gmail.com>
2020-06-15 19:00:23 +0100

    Training

---
+++ train/build_train.py
@@ -0,0 +1,331 @@
+import struct
+import numpy as np
+from keras.layers import Conv2D
+from keras.layers import Input
+from keras.layers import BatchNormalization
+from keras.layers import LeakyReLU
+from keras.layers import ZeroPadding2D
+from keras.layers import UpSampling2D
+from keras.layers.merge import add, concatenate
+from keras.models import Model
+import keras.backend as K
+import tensorflow as tf
+
+def _conv_block(inp, convs, skip=True):
+	x = inp
+	count = 0
+	for conv in convs:
+		if count == (len(convs) - 2) and skip:
+			skip_connection = x
+		count += 1
+		if conv['stride'] > 1: x = ZeroPadding2D(((1,0),(1,0)))(x) # PADDING EN HAUT ET A GAUCHE (COMME DANS LE PAPER)
+		x = Conv2D(conv['filter'],
+				   conv['kernel'],
+				   strides=conv['stride'],
+				   padding='valid' if conv['stride'] > 1 else 'same', # PADDING EN HAUT ET A GAUCHE (COMME DANS LE PAPER)
+				   name='conv_' + str(conv['layer_idx']),
+				   use_bias=False if conv['bnorm'] else True)(x)
+		if conv['bnorm']: x = BatchNormalization(epsilon=0.001, name='bnorm_' + str(conv['layer_idx']))(x)
+		if conv['leaky']: x = LeakyReLU(alpha=0.1, name='leaky_' + str(conv['layer_idx']))(x)
+	return add([skip_connection, x]) if skip else x
+
+def build_model_net():
+	input_image = Input(shape=(None, None, 3))
+	# Layer  0 => 4
+	x = _conv_block(input_image, [{'filter': 32, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 0},
+								  {'filter': 64, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 1},
+								  {'filter': 32, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 2},
+								  {'filter': 64, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 3}])
+	# Layer  5 => 8
+	x = _conv_block(x, [{'filter': 128, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 5},
+						{'filter':  64, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 6},
+						{'filter': 128, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 7}])
+	# Layer  9 => 11
+	x = _conv_block(x, [{'filter':  64, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 9},
+						{'filter': 128, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 10}])
+	# Layer 12 => 15
+	x = _conv_block(x, [{'filter': 256, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 12},
+						{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 13},
+						{'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 14}])
+	# Layer 16 => 36
+	for i in range(7):
+		x = _conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 16+i*3},
+							{'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 17+i*3}])
+	skip_36 = x
+	# Layer 37 => 40
+	x = _conv_block(x, [{'filter': 512, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 37},
+						{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 38},
+						{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 39}])
+	# Layer 41 => 61
+	for i in range(7):
+		x = _conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 41+i*3},
+							{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 42+i*3}])
+	skip_61 = x
+	# Layer 62 => 65
+	x = _conv_block(x, [{'filter': 1024, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 62},
+						{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 63},
+						{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 64}])
+	# Layer 66 => 74
+	for i in range(3):
+		x = _conv_block(x, [{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 66+i*3},
+							{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 67+i*3}])
+	# Layer 75 => 79
+	x = _conv_block(x, [{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 75},
+						{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 76},
+						{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 77},
+						{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 78},
+						{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 79}], skip=False)
+	# Layer 80 => 82
+	yolo_82 = _conv_block(x, [{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 80},
+							  {'filter':  255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 81}], skip=False)
+	# Layer 83 => 86
+	x = _conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 84}], skip=False)
+	x = UpSampling2D(2)(x)
+	x = concatenate([x, skip_61])
+	# Layer 87 => 91
+	x = _conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 87},
+						{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 88},
+						{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 89},
+						{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 90},
+						{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 91}], skip=False)
+	# Layer 92 => 94
+	yolo_94 = _conv_block(x, [{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 92},
+							  {'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 93}], skip=False)
+	# Layer 95 => 98
+	x = _conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True,   'layer_idx': 96}], skip=False)
+	x = UpSampling2D(2)(x)
+	x = concatenate([x, skip_36])
+	# Layer 99 => 106
+	yolo_106 = _conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 99},
+							   {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 100},
+							   {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 101},
+							   {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 102},
+							   {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 103},
+							   {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 104},
+							   {'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 105}], skip=False)
+	model = Model(input_image, [yolo_82, yolo_94, yolo_106])
+	return model
+
+class WeightReader:
+	def __init__(self, weight_file):
+		with open(weight_file, 'rb') as w_f:
+			major,	= struct.unpack('i', w_f.read(4))
+			minor,	= struct.unpack('i', w_f.read(4))
+			revision, = struct.unpack('i', w_f.read(4))
+			if (major*10 + minor) >= 2 and major < 1000 and minor < 1000:
+				w_f.read(8)
+			else:
+				w_f.read(4)
+			transpose = (major > 1000) or (minor > 1000)
+			binary = w_f.read()
+		self.offset = 0
+		self.all_weights = np.frombuffer(binary, dtype='float32')
+
+	def read_bytes(self, size):
+		self.offset = self.offset + size
+		return self.all_weights[self.offset-size:self.offset]
+
+	def load_weights(self, model):
+		for i in range(106):
+			try:
+				conv_layer = model.get_layer('conv_' + str(i))
+				print("loading weights of convolution #" + str(i))
+				if i not in [81, 93, 105]:
+					norm_layer = model.get_layer('bnorm_' + str(i))
+					size = np.prod(norm_layer.get_weights()[0].shape)
+					beta  = self.read_bytes(size) # bias
+					gamma = self.read_bytes(size) # scale
+					mean  = self.read_bytes(size) # mean
+					var   = self.read_bytes(size) # variance
+					weights = norm_layer.set_weights([gamma, beta, mean, var])
+				if len(conv_layer.get_weights()) > 1:
+					bias   = self.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+					kernel = self.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+					kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+					kernel = kernel.transpose([2,3,1,0])
+					conv_layer.set_weights([kernel, bias])
+				else:
+					kernel = self.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+					kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+					kernel = kernel.transpose([2,3,1,0])
+					conv_layer.set_weights([kernel])
+			except ValueError:
+				print("no convolution #" + str(i))
+
+	def reset(self):
+		self.offset = 0
+
+
+
+def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes):
+
+    num_layers = len(anchors)//3 # égal à 3, puisqu'il y a 9 anchors,  3 anchors pour chaque layer de prediction.
+    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] # masks correspondants des anchors.
+	# Convertir les listes données en args en np.array
+    true_boxes = np.array(true_boxes, dtype='float32')
+    input_shape = np.array(input_shape, dtype='int32')
+	# Extraire les coordonnées des points de centre.
+    boxes_xy = (true_boxes[..., 0:2] + true_boxes[..., 2:4]) // 2
+	# Extraire la hauteur et la largeur.
+    boxes_wh = true_boxes[..., 2:4] - true_boxes[..., 0:2]
+
+    true_boxes[..., 0:2] = boxes_xy/input_shape[::-1]
+    true_boxes[..., 2:4] = boxes_wh/input_shape[::-1]
+
+    m = true_boxes.shape[0]
+    grid_shapes = [input_shape//{0:32, 1:16, 2:8}[l] for l in range(num_layers)]
+    y_true = [np.zeros((m,grid_shapes[l][0],grid_shapes[l][1],len(anchor_mask[l]),5+num_classes),
+        dtype='float32') for l in range(num_layers)]
+
+    # Étender la dimension pour faire le broadcast(pas besoin de boucle for).
+    anchors = np.expand_dims(anchors, 0)
+    anchor_maxes = anchors / 2.
+    anchor_mins = - anchor_maxes
+    valid_mask = boxes_wh[..., 0] > 0
+
+    for b in range(m):
+        # Éliminer les lignes nulles.
+        wh = boxes_wh[b, valid_mask[b]]
+        if len(wh)==0: continue
+        # Étender la dimension pour faire le broadcast(pas besoin de boucle for).
+        wh = np.expand_dims(wh, -2)
+        box_maxes = wh / 2.
+        box_mins = -box_maxes
+
+        intersect_mins = np.maximum(box_mins, anchor_mins)
+        intersect_maxes = np.minimum(box_maxes, anchor_maxes)
+        intersect_wh = np.maximum(intersect_maxes - intersect_mins, 0.)
+        intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+        box_area = wh[..., 0] * wh[..., 1]
+        anchor_area = anchors[..., 0] * anchors[..., 1]
+        iou = intersect_area / (box_area + anchor_area - intersect_area)
+
+        # Trouver le meilleur anchor pour chacun des boxes.
+        best_anchor = np.argmax(iou, axis=-1)
+
+        for t, n in enumerate(best_anchor):
+            for l in range(num_layers):
+                if n in anchor_mask[l]:
+                    i = np.floor(true_boxes[b,t,0]*grid_shapes[l][1]).astype('int32')
+                    j = np.floor(true_boxes[b,t,1]*grid_shapes[l][0]).astype('int32')
+                    k = anchor_mask[l].index(n)
+                    c = true_boxes[b,t, 4].astype('int32')
+                    y_true[l][b, j, i, k, 0:4] = true_boxes[b,t, 0:4]
+                    y_true[l][b, j, i, k, 4] = 1
+                    y_true[l][b, j, i, k, 5+c] = 1
+
+    return y_true
+
+def yolo_loss(args, anchors, num_classes, ignore_thresh=.5, print_loss=False):
+
+    num_layers = len(anchors)//3 # égal à 3, puisqu'il y a 9 anchors,  3 anchors pour chaque layer de prediction.
+    yolo_outputs = args[:num_layers]
+    y_true = args[num_layers:]
+    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] # masks correspondants des anchors.
+    input_shape = K.cast(K.shape(yolo_outputs[0])[1:3] * 32, K.dtype(y_true[0]))
+    grid_shapes = [K.cast(K.shape(yolo_outputs[l])[1:3], K.dtype(y_true[0])) for l in range(num_layers)]
+    loss = 0 # initialiser la variable de loss à zéro.
+    m = K.shape(yolo_outputs[0])[0] # le batch-size.
+    mf = K.cast(m, K.dtype(yolo_outputs[0]))
+
+    for l in range(num_layers):
+		# Probabilités réelles:  1 s'il existe un objet, 0 sinon. (extraites du dataset)
+        object_mask = y_true[l][..., 4:5]
+		# Probabilités réelles: 1 s'il appartient à la classe i, 0 sinon. (extraites du dataset)
+        true_class_probs = y_true[l][..., 5:]
+
+        grid, raw_pred, pred_xy, pred_wh = yolo_head(yolo_outputs[l],
+             anchors[anchor_mask[l]], num_classes, input_shape, calc_loss=True)
+		# Concaténer les prédictions xy et wh en un seul array : pred_box.
+        pred_box = K.concatenate([pred_xy, pred_wh])
+
+        # Données de box brutes pour calcler le coût.
+        raw_true_xy = y_true[l][..., :2]*grid_shapes[l][::-1] - grid
+        raw_true_wh = K.log(y_true[l][..., 2:4] / anchors[anchor_mask[l]] * input_shape[::-1])
+        raw_true_wh = K.switch(object_mask, raw_true_wh, K.zeros_like(raw_true_wh)) # avoid log(0)=-inf
+        box_loss_scale = 2 - y_true[l][...,2:3]*y_true[l][...,3:4]
+
+        # Trouver ignore_mask, en itérant par chaque batch.
+        ignore_mask = tf.TensorArray(K.dtype(y_true[0]), size=1, dynamic_size=True)
+        object_mask_bool = K.cast(object_mask, 'bool')
+        def loop_body(b, ignore_mask):
+            true_box = tf.boolean_mask(y_true[l][b,...,0:4], object_mask_bool[b,...,0])
+            iou = box_iou(pred_box[b], true_box)
+            best_iou = K.max(iou, axis=-1)
+            ignore_mask = ignore_mask.write(b, K.cast(best_iou<ignore_thresh, K.dtype(true_box)))
+            return b+1, ignore_mask
+        _, ignore_mask = tf.while_loop(lambda b,*args: b<m, loop_body, [0, ignore_mask])
+        ignore_mask = ignore_mask.stack()
+        ignore_mask = K.expand_dims(ignore_mask, -1)
+
+        # K.binary_crossentropy utile pour éviter le débordement de l'exp().
+        xy_loss = object_mask * box_loss_scale * K.binary_crossentropy(raw_true_xy, raw_pred[...,0:2], from_logits=True)
+        wh_loss = object_mask * box_loss_scale * 0.5 * K.square(raw_true_wh-raw_pred[...,2:4])
+        confidence_loss = object_mask * K.binary_crossentropy(object_mask, raw_pred[...,4:5], from_logits=True)+ \
+            (1-object_mask) * K.binary_crossentropy(object_mask, raw_pred[...,4:5], from_logits=True) * ignore_mask
+        class_loss = object_mask * K.binary_crossentropy(true_class_probs, raw_pred[...,5:], from_logits=True)
+
+        xy_loss = K.sum(xy_loss) / mf
+        wh_loss = K.sum(wh_loss) / mf
+        confidence_loss = K.sum(confidence_loss) / mf
+        class_loss = K.sum(class_loss) / mf
+        loss += xy_loss + wh_loss + confidence_loss + class_loss
+        if print_loss:
+            loss = tf.Print(loss, [loss, xy_loss, wh_loss, confidence_loss, class_loss, K.sum(ignore_mask)], message='loss: ')
+    return loss
+# Cette fonction convertit la sortie brute en paramètres de boîte réels.
+def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
+
+    num_anchors = len(anchors)
+    # Retailler à  (batch, height, width, num_anchors, box_params).
+    anchors_tensor = K.reshape(K.constant(anchors), [1, 1, 1, num_anchors, 2])
+
+    grid_shape = K.shape(feats)[1:3] # Longueur, Largeur.
+    grid_y = K.tile(K.reshape(K.arange(0, stop=grid_shape[0]), [-1, 1, 1, 1]),
+        [1, grid_shape[1], 1, 1])
+    grid_x = K.tile(K.reshape(K.arange(0, stop=grid_shape[1]), [1, -1, 1, 1]),
+        [grid_shape[0], 1, 1, 1])
+    grid = K.concatenate([grid_x, grid_y])
+    grid = K.cast(grid, K.dtype(feats))
+
+    feats = K.reshape(
+        feats, [-1, grid_shape[0], grid_shape[1], num_anchors, num_classes + 5])
+
+    # Répatir les prédictions sur toutes la grilles.
+    box_xy = (K.sigmoid(feats[..., :2]) + grid) / K.cast(grid_shape[::-1], K.dtype(feats))
+    box_wh = K.exp(feats[..., 2:4]) * anchors_tensor / K.cast(input_shape[::-1], K.dtype(feats))
+    box_confidence = K.sigmoid(feats[..., 4:5])
+    box_class_probs = K.sigmoid(feats[..., 5:])
+
+    if calc_loss == True:
+        return grid, feats, box_xy, box_wh
+    return box_xy, box_wh, box_confidence, box_class_probs
+
+
+def box_iou(b1, b2):
+
+    # Étender la dimension pour faire le broadcast(pas besoin de boucle for).
+    b1 = K.expand_dims(b1, -2)
+    b1_xy = b1[..., :2]
+    b1_wh = b1[..., 2:4]
+    b1_wh_half = b1_wh/2.
+    b1_mins = b1_xy - b1_wh_half
+    b1_maxes = b1_xy + b1_wh_half
+
+    # Étender la dimension pour faire le broadcast(pas besoin de boucle for).
+    b2 = K.expand_dims(b2, 0)
+    b2_xy = b2[..., :2]
+    b2_wh = b2[..., 2:4]
+    b2_wh_half = b2_wh/2.
+    b2_mins = b2_xy - b2_wh_half
+    b2_maxes = b2_xy + b2_wh_half
+
+    intersect_mins = K.maximum(b1_mins, b2_mins)
+    intersect_maxes = K.minimum(b1_maxes, b2_maxes)
+    intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+    b1_area = b1_wh[..., 0] * b1_wh[..., 1]
+    b2_area = b2_wh[..., 0] * b2_wh[..., 1]
+    iou = intersect_area / (b1_area + b2_area - intersect_area)
+
+    return iou
